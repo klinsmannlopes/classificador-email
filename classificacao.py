e185d08b6ce49b5eb1aa6adda9747bb4c68fadd1
klinsmann <lopesklinsmann@outlook.com>
2019-05-05 20:05:23 -0300

    vendo taxa de acerto

--- classificacao.py
+++ classificacao.py
@@ -22,7 +22,20 @@ modelo.fit(dados, marcacoes)
 #prevendo resultado
 misterioso1 = [1, 1, 1]
 misterioso2 = [1, 0, 0]
+misterioso3 = [0, 0, 1]
 
-teste = [misterioso1, misterioso2]
+teste = [misterioso1, misterioso2, misterioso3]
+marcacoes_teste = [-1, 1, -1]
 
-print(modelo.predict(misterioso1))
+resultado = modelo.predict(teste)
+print(resultado)
+diferenca = resultado - marcacoes_teste
+
+acertos = [d for d in diferenca if d==0]
+
+total_acertos = len(acertos)
+total_elementos = len(teste)
+
+taxa_de_acerto = 100 * total_acertos / total_elementos
+
+print(taxa_de_acerto)
