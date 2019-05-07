041e742b6d1ac3c6689161da7e8f56d007b67f66
klinsmann <lopesklinsmann@outlook.com>
2019-05-06 23:29:52 -0300

    abrindo e separando dados de arquivo csv

---
+++ classifica_acessos.py
@@ -0,0 +1,5 @@
+from dados import carregar_acesso
+
+x, y = carregar_acesso()
+
+print(y)
---
+++ dados.py
@@ -0,0 +1,17 @@
+import csv
+
+def carregar_acesso():
+
+    x = []
+    y = []
+
+    arquivo = open('acesso.csv', 'r')
+    leitor = csv.reader(arquivo)
+
+    next(leitor)
+    for home,como_funciona,contato,comprou in leitor:
+        dados = [int(home),int(como_funciona),int(contato)]
+        x.append(dados)
+        y.append([int(comprou)])
+
+    return x, y
