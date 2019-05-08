68903602ea02912de7ecb418ea8afcc691cff822
klinsmann <lopesklinsmann@outlook.com>
2019-05-07 22:26:23 -0300

    verificando taxa de acerto

--- classifica_acessos.py
+++ classifica_acessos.py
@@ -9,3 +9,15 @@ modelo = MultinomialNB()
 modelo.fit(x, y)
 
 print(modelo.predict([[1, 0, 1], [0, 1, 0]]))
+
+resultado = modelo.predict(x)
+diferenca = resultado - y
+
+acertos = [d for d in diferenca if d == 0]
+total_elementos = len(x)
+total_acertos   = len(acertos)
+
+porcentagem_acertos = (100 * total_acertos) / \
+                      total_elementos
+
+print(porcentagem_acertos)
