3a46875ea1c920c94d30fef79a6f1c785a923bd6
klinsmann <lopesklinsmann@outlook.com>
2019-05-07 22:40:51 -0300

    separando dados de modelagem dos de testes

--- classifica_acessos.py
+++ classifica_acessos.py
@@ -4,20 +4,25 @@ from sklearn.naive_bayes import MultinomialNB
 
 x, y = carregar_acesso()
 
+treino_dados = x[:90]
+treino_marcacoes = y[:90]
+
+teste_dados = x[-9:]
+teste_marcacoes = y[-9:]
+
+
+
 modelo = MultinomialNB()
+modelo.fit(treino_dados, treino_marcacoes)
 
-modelo.fit(x, y)
-
-print(modelo.predict([[1, 0, 1], [0, 1, 0]]))
-
-resultado = modelo.predict(x)
-diferenca = resultado - y
+resultado = modelo.predict(teste_dados)
+diferenca = resultado - teste_marcacoes
 
 acertos = [d for d in diferenca if d == 0]
-total_elementos = len(x)
+total_elementos = len(teste_dados)
 total_acertos   = len(acertos)
 
 porcentagem_acertos = (100 * total_acertos) / \
                       total_elementos
 
-print(porcentagem_acertos)
+print('{:.2f}'.format(porcentagem_acertos))
