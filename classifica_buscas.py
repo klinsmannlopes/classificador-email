6b2f0047cb7c4f392ed7ffb7ff427c23b4af654b
klinsmann <lopesklinsmann@outlook.com>
2019-05-11 19:27:38 -0300

    lógica criada para diferenciar pesquisas

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -1,16 +1,26 @@
-from dados import carregar_buscas
+import pandas as pd
+df = pd.read_csv('busca.csv')
+
+x_df = df[['home', 'busca', 'logado']]
+y_df = df['comprou']
+
+Xdummies_df = pd.get_dummies(x_df)
+
+x = Xdummies_df.values
+y = y_df.values
+
+porcetagem_treino = 0.9
+
+tamanho_treino = int(0.9 * len(y))
+tamanho_teste = int(len(y) - tamanho_treino)
+
+treino_dados = x[:tamanho_treino]
+treino_marcacoes = y[:tamanho_treino]
+
+teste_dados = x[-tamanho_teste:]
+teste_marcacoes = y[-tamanho_teste:]
+
 from sklearn.naive_bayes import MultinomialNB
-
-x, y = carregar_buscas()
-
-
-treino_dados = x[:90]
-treino_marcacoes = y[:90]
-
-teste_dados = x[-9:]
-teste_marcacoes = y[-9:]
-
-
 
 modelo = MultinomialNB()
 modelo.fit(treino_dados, treino_marcacoes)
@@ -19,10 +29,8 @@ resultado = modelo.predict(teste_dados)
 diferenca = resultado - teste_marcacoes
 
 acertos = [d for d in diferenca if d == 0]
-total_elementos = len(teste_dados)
-total_acertos   = len(acertos)
+total_de_acertos = len(acertos)
+total_de_elementos = len(teste_dados)
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
 
-porcentagem_acertos = (100 * total_acertos) / \
-                      total_elementos
-
-print('{:.2f}'.format(porcentagem_acertos))
+print('{:.2f}'.format(taxa_de_acerto))
